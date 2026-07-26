010be34a7afc1ccbcb1db127751455b998819a05
brettmorinaka <32185742+brettmorinaka@users.noreply.github.com>
2026-07-25 20:26:34 -0700

    remove db user and password

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres:fAZLZ7q8yrJChg6teRA7NmL6d@localhost:5432/cafe_fausse",
+        "postgresql://{user}:{password}@localhost:5432/cafe_fausse",
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TOTAL_TABLES = 30
